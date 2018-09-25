6118051374fb38a06e27c70d31c6f6ebdec990bc
andrey239 <andrey.smirdin@mail.ru>
2018-09-25 19:51:59 +0300

    Exponential distribution added.

--- 3/main.py
+++ 3/main.py
@@ -1,26 +1,35 @@
 import math
 import random
+
 import matplotlib.pyplot as plt
 
-THETA = 50
-EXPERIMENTS = 1000
-MAX_K = 180
+THETA = 10
+EXPERIMENTS = 100
+MAX_K = 100
+SERIES = 100
 
-def estimate_parameter(k):
-    sum = 0
-    for _ in range(EXPERIMENTS):
-        sum += math.pow(random.uniform(0, THETA), k)
-    sum /= EXPERIMENTS
-    return pow((k + 1) * sum, 1 / k)
+
+def estimate_uniform(k):
+    res = sum([random.uniform(0, THETA) ** k for _ in range(EXPERIMENTS)]) / EXPERIMENTS
+    return ((k + 1) * res) ** (1 / k)
+
+
+def estimate_exp(k):
+    res = sum([random.expovariate(THETA) ** k for _ in range(EXPERIMENTS)]) / EXPERIMENTS
+    return (math.factorial(k) / res) ** (1 / k)
+
+
+def do(estimate_param, plot_name):
+    results = []
+    for k in range(1, MAX_K):
+        deviation = sum([(THETA - estimate_param(k)) ** 2 for _ in range(SERIES)])
+        results.append((deviation / SERIES) ** 0.5)
+
+    plt.clf()
+    plt.plot([i for i in range(1, MAX_K)], results)
+    plt.savefig(plot_name)
 
 
 if __name__ == "__main__":
-    results = []
-    for k in range(1, MAX_K):
-        val = estimate_parameter(k)
-        print(val)
-        results.append((THETA - val) * (THETA - val))
-
-    plt.plot([i for i in range(1, MAX_K)], results)
-    plt.savefig('result.png')
-    plt.show()
+    do(estimate_uniform, 'uniform.png')
+    do(estimate_exp, 'exponential.png')
